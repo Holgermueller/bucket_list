b262c05fc7654daafbfe7538b4d14bc88292dcab
Holger Mueller <9jholger@gmail.com>
2021-04-25 15:54:01 -0500

    set up navigation

--- src/Components/BucketList.component.js
+++ src/Components/BucketList.component.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
+import { Grid, Card, Divider, Typography } from "@material-ui/core";
 
 export default class BucketList extends Component {
   render() {
-    return <div>This is the home page</div>;
+    return (
+      <div>
+        <Card>
+          <Typography variant="h6">Welcome to your Bucket List</Typography>
+        </Card>
+      </div>
+    );
   }
 }
--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -1,21 +1,20 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { Paper, Tabs, Tab } from "@material-ui/core";
 
 export default class NavBar extends Component {
   render() {
     return (
       <header>
-        <Link to="/">
-          <h2 component={Link}>Mylist</h2>
-        </Link>
+        <Paper>
+          <Tabs centered>
+            <Tab label="My List" to="/" component={Link} />
 
-        <Link to="/create">
-          <h2 component={Link}>Create Item</h2>
-        </Link>
+            <Tab label="Create Item" to="/create" component={Link} />
 
-        <Link to="/completed">
-          <h2 component={Link}>Completed Items</h2>
-        </Link>
+            <Tab label="Completed Items" to="/completed" component={Link} />
+          </Tabs>
+        </Paper>
       </header>
     );
   }
