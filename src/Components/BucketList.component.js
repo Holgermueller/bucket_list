5db610cec127066b3e75bd589c379b022caf323a
Holger Mueller <9jholger@gmail.com>
2021-04-26 10:58:25 -0500

    basic scaffodl submit item page

--- src/Components/BucketList.component.js
+++ src/Components/BucketList.component.js
@@ -1,13 +1,49 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import { Grid, Card, Divider, Typography } from "@material-ui/core";
 
+const ItemOnList = (props) => (
+  <Card>
+    <Typography></Typography>
+    <Divider />
+    Status: <Typography></Typography>
+    <Link>Edit</Link>
+  </Card>
+);
+
 export default class BucketList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      itemsOnList: [],
+    };
+  }
+
+  loadBucketListItems = () => {
+    return this.state.itemsOnList.map(function (listItem, i) {
+      return <ItemOnList itemOnList={listItem} key={i} />;
+    });
+  };
+
   render() {
     return (
       <div>
         <Card>
           <Typography variant="h6">Welcome to your Bucket List</Typography>
         </Card>
+
+        <div>
+          {this.state.itemsOnList ? (
+            <Grid>{this.loadBucketListItems()}</Grid>
+          ) : (
+            <Grid>
+              <Card>
+                <h1>Enter something already!!</h1>
+                <h2>The clock is ticking!!</h2>
+              </Card>
+            </Grid>
+          )}
+        </div>
       </div>
     );
   }
--- src/Components/CreateItem.component.js
+++ src/Components/CreateItem.component.js
@@ -1,7 +1,45 @@
 import React, { Component } from "react";
+import { Card, TextField, Button } from "@material-ui/core";
+
+const cardStyles = {
+  textAlign: "center",
+};
+
+const formStyles = {
+  padding: "5%",
+  display: "inline-grid",
+};
 
 export default class CreateItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      bucketListItem_name: "",
+      error: "",
+    };
+  }
+
   render() {
-    return <div>Add items to your list here.</div>;
+    return (
+      <div>
+        <Card style={cardStyles}>
+          <h3>Add and item to your Bucket List!</h3>
+
+          <hr />
+
+          <form style={formStyles}>
+            <TextField
+              style={{ width: 250 }}
+              placeholder="What would you like to do?"
+              margin="normal"
+            />
+
+            <Button>Submit</Button>
+            <Button>Cancel</Button>
+          </form>
+        </Card>
+      </div>
+    );
   }
 }
